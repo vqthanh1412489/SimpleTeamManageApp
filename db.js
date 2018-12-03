0f6703e4797e6c27a7ffe183518ec672f4d1568c
vqthanh1412489 <vqthanh1412489@gmail.com>
2018-12-03 21:09:08 +0700

    Init

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongooes = require('mongoose');
 
-const uri = 'mongodb://localhost/test';
+const uri = 'mongodb://vqthanh1412489:sinhvien1T@ds123834.mlab.com:23834/testpv3009';
 
 mongooes.connect(uri, { useNewUrlParser: true })
 .then(() => console.log('Connect Success'))
