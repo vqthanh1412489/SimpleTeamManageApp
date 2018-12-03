dcee9aec8433466d7167c11045ffddba496ad2a2
vqthanh1412489 <vqthanh1412489@gmail.com>
2018-12-03 23:25:09 +0700

    add cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+var cors = require('cors');
 
 const { userRouter } = require('./controllers/user.router');
 const { projectRouter } = require('./controllers/project.router');
@@ -9,5 +10,6 @@ require('./db');
 
 app.use('/user', userRouter);
 app.use('/project', projectRouter);
+app.use(cors());
 
 module.exports = { app };
